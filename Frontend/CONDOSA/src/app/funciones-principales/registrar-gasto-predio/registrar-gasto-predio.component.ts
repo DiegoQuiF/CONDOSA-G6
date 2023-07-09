c0396fff78039bdb3d2c3048f0caa2946464cc44
Jul1o21 <137314058+CubasJ@users.noreply.github.com>
2023-07-09 02:22:26 -0500

    sum
    desc

--- Frontend/CONDOSA/src/app/funciones-principales/registrar-gasto-predio/registrar-gasto-predio.component.ts
+++ Frontend/CONDOSA/src/app/funciones-principales/registrar-gasto-predio/registrar-gasto-predio.component.ts
@@ -10,93 +10,74 @@ import { Predio } from 'src/app/models/predio';
   styleUrls: ['./registrar-gasto-predio.component.css']
 })
 export class RegistrarGastoPredioComponent implements OnInit {
-  
-  constructor(private connBackend:ConnBackendService){}
 
-  tipoGastoArray:TipoGastos[] = [];
-  selectedItemTipoGasto:String = 'Seleccione';
-  isActiveTipoGasto:boolean = false;
+  constructor(private connBackend: ConnBackendService) { }
 
-  descripGastoArray:DescripGastos[] = [];
-  selectedItemDescripGasto:String = 'Seleccione';
+  tipoGastoArray: Array<TipoGastos> = new Array<TipoGastos>();
+  selectedItemTipoGasto: String = 'Seleccione';
+  isActiveTipoGasto: boolean = false;
+
+  descripGastoArray: Array<DescripGastos> = new Array<DescripGastos>();
+  selectedItemDescripGasto: String = 'Seleccione';
   isActiveDescripGasto: boolean = false;
 
-  predioArray:Predio[] = [];
-
-  predioCuota:string = 'Nombre del predio';
-  periodoCuota:string = 'Periodo del predio';
-
+  predioArray: Array<Predio> = new Array<Predio>();
+  nombrePredio: string = 'Nombre del predio';
+  periodoRegistro: string = 'Periodo del predio';
 
 
+  @Output() mostrarRegistroPredio_OUT = new EventEmitter<boolean>();
+  @Output() estadoRegistroPredio_OUT = new EventEmitter<string>();
+  @Input() id_predio_IN: string = "";
 
-
-  @Output() mostrarRegistroPredio_OUT=new EventEmitter<boolean>();
-  @Output() estadoRegistroPredio_OUT=new EventEmitter<string>();
-  @Input() id_predio_IN:string = "";
-
-  gatosRegistrados: any[] = [
-    { TipoGasto: 'Planilla (portería- áreas comunes- limpieza)', Monto: 305},
+  gatosRegistradosArray: any[] = [
+    { TipoGasto: 'Planilla (portería- áreas comunes- limpieza)', Monto: 305 },
     { TipoGasto: 'Administración y contabilidad ', Monto: 660 },
-    { TipoGasto: 'Teléfono fijo e internet',  Monto: 123},
-    { TipoGasto: 'Consumo de Luz Mensual SS-GG -Suministro 1695605',  Monto: 123 },
-    { TipoGasto: 'Consumo de Luz Mensual BCI -Suministro 1695613',  Monto: 123},
-    { TipoGasto: 'Planilla (portería- áreas comunes- limpieza)', Monto: 305},
+    { TipoGasto: 'Teléfono fijo e internet', Monto: 123 },
+    { TipoGasto: 'Planilla (portería- áreas comunes- limpieza)', Monto: 305 },
     { TipoGasto: 'Administración y contabilidad ', Monto: 660 },
-    { TipoGasto: 'Teléfono fijo e internet',  Monto: 123},
-    { TipoGasto: 'Consumo de Luz Mensual SS-GG -Suministro 1695605',  Monto: 123 },
-    { TipoGasto: 'Consumo de Luz Mensual BCI -Suministro 1695613',  Monto: 123},
-    { TipoGasto: 'Planilla (portería- áreas comunes- limpieza)', Monto: 305},
-    { TipoGasto: 'Administración y contabilidad ', Monto: 660 },
-    { TipoGasto: 'Teléfono fijo e internet',  Monto: 123},
-    { TipoGasto: 'Consumo de Luz Mensual SS-GG -Suministro 1695605',  Monto: 123 },
-    { TipoGasto: 'Consumo de Luz Mensual BCI -Suministro 1695613',  Monto: 123},
-    { TipoGasto: 'Planilla (portería- áreas comunes- limpieza)', Monto: 305},
-    { TipoGasto: 'Administración y contabilidad ', Monto: 660 },
-    { TipoGasto: 'Teléfono fijo e internet',  Monto: 123},
-    { TipoGasto: 'Consumo de Luz Mensual SS-GG -Suministro 1695605',  Monto: 123 },
-    { TipoGasto: 'Consumo de Luz Mensual BCI -Suministro 1695613',  Monto: 123}
-  ]; 
-
-  listaTipoGasto: any[] = ['Gasto Diverso', 'Gasto Laboral', 'Gasto Adm. y Contabilidad'];
-  listaDescripcion: any[] = ['Telefono fijo e internet', 'Planilla', 'Administracion y Contabilidad','Consumo de Luz Mensual SS-GG -Suministro 1695605'];
-  
+    { TipoGasto: 'Teléfono fijo e internet', Monto: 123 },
+    { TipoGasto: 'Consumo de Luz Mensual SS-GG -Suministro 1695605', Monto: 123 },
+    { TipoGasto: 'Consumo de Luz Mensual BCI -Suministro 1695613', Monto: 123 }
+  ];
+ 
   ngOnInit() {
     
+    //AL INICIO CARGAMOS LOS PREDIOS QUE COINCIDEN C
     this.connBackend.getPredio(this.id_predio_IN)
-    .subscribe(data=>{
-      console.log(data)
-      this.predioArray = data.predio;
-      this.predioCuota = this.predioArray[0].predio;
-    },
-    error=>console.log(error));
-    
+      .subscribe(data => {
+        console.log(data)
+        this.nombrePredio = data.predio[0].predio;
+      },
+        error => console.log(error));
+    //AL INICIO CARGAMOS LOS TIPOS DE GASTO  
     this.connBackend.getTipoGastos()
-    .subscribe(data=>{
-      console.log(data)
-      this.tipoGastoArray = data.tipoGastosComunes;
-    },
-    error=>console.log(error));
+      .subscribe(data => {
+        console.log(data)
+        this.tipoGastoArray = data.tipoGastosComunes;
+      },
+        error => console.log(error));
   }
 
-  set_mostrarRegistroPredio(item:boolean){
-    console.log("se cambia a "+ item + "desde el registrar predios");
+  set_mostrarRegistroPredio(item: boolean) {
+    console.log("se cambia a " + item + "desde el registrar predios");
     this.mostrarRegistroPredio_OUT.emit(item);
   }
 
-  finalizarRegistroPredio(){
+  finalizarRegistroPredio() {
     //Aca iria el metodo para modificar la tabla ESTADO_REGISTRO_PREDIO de l BD
     this.estadoRegistroPredio_OUT.emit('finalizado');
-    console.log("Se finaliza del predio de id: "+ this.id_predio_IN)
+    console.log("Se finaliza del predio de id: " + this.id_predio_IN)
   }
 
   selectedTipoGasto(item: TipoGastos): void {    //PERMITE SELECCIONAR EL TIPO DE GASTO Y CERRAR EL CBOX DE PREDIOS
     this.selectedItemTipoGasto = item.descripcion;
     this.connBackend.getDescripGastos(item.id_tipo_gasto)
-    .subscribe(data=>{
-      console.log(data)
-      this.descripGastoArray = data.descripGastosComunes;
-    },
-    error=>console.log(error));
+      .subscribe(data => {
+        console.log(data)
+        this.descripGastoArray = data.descripGastosComunes;
+      },
+        error => console.log(error));
     this.toggleActiveTipoGasto();
   }
 
@@ -111,10 +92,10 @@ export class RegistrarGastoPredioComponent implements OnInit {
   }
 
   toggleActiveDescripGasto(): void {   //PERMITE (PARA EL CBOX DE DESCRIPCION) ACTIVAR SI ESTÁ DESACTIVADO, DESACTIVAR SI ESTÁ ACTIVADO
-    if(this.selectedItemTipoGasto !== "Seleccione"){
+    if (this.selectedItemTipoGasto !== "Seleccione") {
       this.isActiveDescripGasto = !this.isActiveDescripGasto;
     }
-    else{
+    else {
       alert('Seleccione un TIPO DE GASTO.');
     }
   }
